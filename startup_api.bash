2b9900a8229813673c346cb34925abe6d8121db4
Student <example@example.com>
2020-02-04 14:13:37 -0800

    seed dabase, switch server ports

--- startup_api.bash
+++ startup_api.bash
@@ -2,4 +2,5 @@ cd Envision
 bundle install
 rake db:create
 rake db:schema:load
-rails s
+rake db:seed
+rails s -p 3001
--- startup_server.bash
+++ startup_server.bash
@@ -2,4 +2,4 @@ cd Envision_api
 bundle install
 rake db:create
 rake db:schema:load
-rails s -p 3001
+rails s 
